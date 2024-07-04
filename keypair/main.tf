05e99a942257ca6eb121efaadc5c9f04342590ed
devops <bharadwajsai220@gmail.com>
2024-07-05 02:18:39 +0530

    keypair/

---
+++ keypair/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_key_pair" "test" {
+  key_name   = "task"  # Replace with your desired key name
+  public_key =  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 " 
+
+
+}
+
+resource "aws_instance" "test2" {
+    ami = "ami-0604d81f2fd264c7b"
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.test.key_name
+  
+}
